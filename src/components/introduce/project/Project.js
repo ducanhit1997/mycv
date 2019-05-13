e95408112dab03de3879b820b082eaffd84df946
ducanhit1997 <MY_NAME@example.com>
2019-05-13 11:15:42 +0700

    update

--- src/components/introduce/project/Project.js
+++ src/components/introduce/project/Project.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import { Timeline, Icon } from 'antd';
 import './../style.css';
+
 class Project extends Component {
     render() {
         return (
-            <div className="project">
-                <Timeline mode="alternate">
-                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                    <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
-                    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</Timeline.Item>
-                    <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
-                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>Technical testing 2015-09-01</Timeline.Item>
-                </Timeline>
-            </div>
+            <div className="main-timeline">
+                <div className="timeline">
+                <a href="#" className="timeline-content">
+                    <span className="year">2018</span>
+                    <h3 className="title">Web Designer</h3>
+                    <p className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+                    </p>
+                </a>
+                </div>
+                <div className="timeline">
+                <a href="#" className="timeline-content">
+                    <span className="year">2017</span>
+                    <h3 className="title">Web Developer</h3>
+                    <p className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+                    </p>
+                </a>
+                </div>
+                <div className="timeline">
+                <a href="#" className="timeline-content">
+                    <span className="year">2017</span>
+                    <h3 className="title">Web Developer</h3>
+                    <p className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+                    </p>
+                </a>
+                </div>
+                <div className="timeline">
+                <a href="#" className="timeline-content">
+                    <span className="year">2017</span>
+                    <h3 className="title">Web Developer</h3>
+                    <p className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+                    </p>
+                </a>
+                </div>
+          </div>
         );
     }
 }
